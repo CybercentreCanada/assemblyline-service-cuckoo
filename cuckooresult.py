48f9f8edbbc723af6769702c9fd044bbab08e824
sgaron-cse <sgaron.cse@gmail.com>
2017-05-30 11:53:29 -0400

    Retry cuckoo on all errors

--- cuckooresult.py
+++ cuckooresult.py
@@ -182,7 +182,7 @@ def process_debug(debug, al_result, classification):
                 # Initialization Error - restart the docker container
                 error_res.add_line(error)
                 if "analysis hit the critical timeout" not in err_str:
-                    raise RecoverableError("An error prevented cuckoo to get complete results, retrying...")
+                    raise RecoverableError("An error prevented cuckoo from generating complete results: %s" % safe_str(error))
         if len(error_res.body) > 0:
             al_result.add_section(error_res)
     return failed
