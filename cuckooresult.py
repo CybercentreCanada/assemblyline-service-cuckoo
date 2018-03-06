02b58e3c8b72f581d823d1a2728ab9324c7417bd
sgaron-cse <sgaron.cse@gmail.com>
2018-03-06 10:52:14 -0500

    Adjust memory footprint Cleanup warnings and errors

--- cuckooresult.py
+++ cuckooresult.py
@@ -18,7 +18,7 @@ from al_services.alsvc_cuckoo.whitelist import wlist_check_ip, wlist_check_domai
 
 CLASSIFICATION = forge.get_classification()
 
-UUID_RE = re.compile(r"\{([0-9A-Fa-f]{8}-(?:[0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12})\}")
+UUID_RE = re.compile(r"{([0-9A-Fa-f]{8}-(?:[0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12})\}")
 USER_SID_RE = re.compile(r"S-1-5-21-\d+-\d+-\d+-\d+")
 WIN_FILE_RE = re.compile(r"Added new file to list with path: (\w:(?:\\[a-zA-Z0-9_\-. $]+)+)")
 DROIDMON_CONN_RE = re.compile(r"([A-Z]{3,5}) (https?://([a-zA-Z0-9.\-]+):?([0-9]{2,5})?([^ ]+)) HTTP/([0-9.]+)")
