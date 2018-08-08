7ad4a6333d42ab19732fc3325d7f4ebe0b118ca7
David <jdv.cse@gmail.com>
2018-08-08 16:06:32 -0400

    more work on testing script. adding cli options

--- cuckoo_tests.py
+++ cuckoo_tests.py
@@ -19,13 +19,38 @@ from assemblyline.al.service.service_driver import ServiceDriver
 from assemblyline.common.logformat import AL_LOG_FORMAT
 
 import al_services.alsvc_cuckoo.cuckoo
+import al_services.alsvc_cuckoo.cuckoo_managers
 import pprint
 import requests
 import platform
 import json
+import tempfile
+import shutil
+import os
+import hashlib
+from Crypto.PublicKey import RSA
+from paramiko import RSAKey
+import argparse
 
+try:
+    import sshtunnel
+except ImportError:
+    print "Error importing sshtunnel. Make sure it's installed with 'pip install sshtunnel'"
+    sys.exit()
 
 config = forge.get_config()
+
+# Used for comparing file contents
+HASH_BLOCKSIZE = 65536
+
+# Available tests. These should all be functions on the CuckooTesting class
+_AVAILABLE_TESTS = [
+    "check_qcow2",
+    "check_cuckoo_status",
+    "is_cuckoo_ready",
+    "compare_ubuntu_versions",
+    "list_vms"
+]
 
 class CuckooTesting:
 
@@ -35,11 +60,92 @@ class CuckooTesting:
     def __init__(self):
         self.log = logging.getLogger("assemblyline.cuckoo.testing")
 
-
         self.log.info("Looking up service config")
         self.svc_cfg = forge.get_datastore().get_service(self._SVC_CLASS.SERVICE_NAME).get("config", {})
         self.log.debug("Service configuration: \n%s" % pprint.pformat(self.svc_cfg))
+        self.service = None
 
+    def __del__(self):
+        if self.service is not None:
+            self.service.stop_service()
+
+    def check_qcow2(self, cleanup=True):
+        """
+        Checks to make sure the qcow2 (disk) and XML virtual machine
+        definition files all match up.
+        """
+        new_cfg = self.svc_cfg.copy()
+        old_disk_root = self.svc_cfg.get("LOCAL_DISK_ROOT")
+        new_disk_root = "cuckoo_testing_disks/"
+        new_cfg["LOCAL_DISK_ROOT"] = new_disk_root
+
+        old_vm_meta = self.svc_cfg.get("LOCAL_VM_META_ROOT")
+        new_vm_meta = "var/cuckoo_meta_testing"
+        new_cfg["LOCAL_VM_META_ROOT"] = new_vm_meta
+
+        # Create a vmm object
+        self.log.info("Creating a CuckooVmManager with modified configuration, downloading disks and configuration to "
+                      "new location")
+        vmm = al_services.alsvc_cuckoo.cuckoo_managers.CuckooVmManager(new_cfg)
+        # Download disks
+        vmm.download_data()
+
+        ##
+
+        for root_path in [vmm.local_meta_root, vmm.local_vm_root]:
+
+            if root_path == vmm.local_meta_root:
+                self.log.info("Comparing XML Virtual Machine definitions")
+                file_ext = ".xml"
+            elif root_path == vmm.local_vm_root:
+                self.log.info("Comparing qcow2 virtual disks")
+                file_ext = ".qcow2"
+            all_files_matched = True
+
+            for dirpath, dirnames, filenames in os.walk(root_path):
+                for fn in filenames:
+                    if fn.lower().endswith(file_ext):
+                        new_path = os.path.join(dirpath,fn)
+                        old_path = os.path.join(dirpath.replace(new_vm_meta, old_vm_meta), fn)
+
+                        self.log.debug("Checking %s" % new_path)
+                        if not os.path.exists(old_path):
+                            self.log.error("Can't find matching file in normal working directory: %s. Newly downloaded: %s" % (old_path, new_path))
+                            continue
+
+                        # Compute hashes this way so we handle large files in a reasonable way
+                        new_hash = hashlib.sha256()
+                        old_hash = hashlib.sha256()
+                        with open(new_path, "rb") as new_fh:
+                            for chunk in iter(lambda: new_fh.read(HASH_BLOCKSIZE), b''):
+                                new_hash.update(chunk)
+                        with open(old_path, "rb") as old_fh:
+                            for chunk in iter(lambda: old_fh.read(HASH_BLOCKSIZE), b''):
+                                old_hash.update(chunk)
+
+                        if new_hash.digest() != old_hash.digest():
+
+                            self.log.error("Mismatched file contents between %(old_path)s and %(new_path)s. "
+                                           "This may be caused by a directory permission issue, or changed backing qcow2 disk. "
+                                           "Try manually overwriting or just deleting %(old_path)s, "
+                                           "The service should re-download from the support server" %
+                                           {
+                                               "old_path": old_path,
+                                               "new_path": new_path
+                                           }
+                                           )
+                            all_files_matched = False
+
+            if all_files_matched:
+                self.log.info("GOOD. All files match.")
+
+        # Cleanup
+        if cleanup:
+            self.log.info("Cleaning up downloaded files")
+            shutil.rmtree(vmm.local_vm_root)
+            shutil.rmtree(vmm.local_meta_root)
+        else:
+            self.log.warning("Didn't clean up VM disks and configuration files. These directories should be deleted: %s , %s" % (vmm.local_meta_root, vmm.local_vm_root))
     def start_service(self):
         self.log.info("Starting service...")
 
@@ -48,6 +154,9 @@ class CuckooTesting:
         self.monkey_patch()
         self.service = self._SVC_CLASS(self.svc_cfg)  # type: al_services.alsvc_cuckoo.cuckoo.Cuckoo
         self.service.start_service()
+
+        self.log.info("Service Started. Using docker container '%s'. " 
+                      "It may take a few moments for components within the docker container to start up." % self.service.cm.name)
 
 
     def monkey_patch(self):
@@ -86,9 +195,10 @@ class CuckooTesting:
     def check_cuckoo_status(self):
         """
         Check the cuckoo/status REST endpoint
-
-        :return:
         """
+
+        if self.service is None:
+            self.start_service()
 
         base_url = "http://%s:%s" % (self.service.cuckoo_ip, al_services.alsvc_cuckoo.cuckoo.CUCKOO_API_PORT)
         full_url = "%s/cuckoo/status" % base_url
@@ -120,9 +230,10 @@ class CuckooTesting:
     def compare_ubuntu_versions(self):
         """
         Compare the version of ubuntu running on host against the one running inside docker
-
-        :return:
         """
+
+        if self.service is None:
+            self.start_service()
 
         (host_dist, host_osver, host_relname) = platform.linux_distribution()
 
@@ -149,9 +260,36 @@ class CuckooTesting:
                 self.log.error("Host is running a newer OS than docker. This is not recommended (VMs created on the "
                                "host will probably not run inside docker)")
 
-    def run_tests(self):
+    def list_vms(self):
+        """
+        Get a list of virtual machines defined within the docker container
+        """
 
-        self.check_cuckoo_status()
+        if self.service is None:
+            self.start_service()
+
+        cmd = """docker exec %s virsh list --all --name""" % self.service.cm.name
+        self.log.info("Trying to get a list of VMs using command: %s" % cmd)
+        stdout, stderr = self.service.cm._run_cmd(cmd)
+        if len(stderr) > 0:
+            self.log.error("Problem getting list of virtual machines, using command '%s'. "
+                           "Maybe an issue with libvirt inside the docker container? "
+                           "STDERR from command: %s" % (cmd, stderr))
+
+        self.vm_names = [x.strip() for x in stdout.splitlines() if len(x) > 0]
+        if len(self.vm_names) == 0:
+            self.log.error("No virtual machines found within docker container.")
+
+        else:
+            self.log.info("Found the following VMs: %s", ",".join(self.vm_names))
+
+    def is_cuckoo_ready(self):
+        """
+        Checks status of the service method Cuckoo.is_cuckoo_ready
+        """
+
+        if self.service is None:
+            self.start_service()
 
         cuckoo_ready = self.service.is_cuckoo_ready()
         if cuckoo_ready:
@@ -160,28 +298,98 @@ class CuckooTesting:
             self.log.error("Cuckoo.is_cuckoo_ready() check failed. "
                            "There are potential issues with docker or the cuckoo API")
 
-        #
-        # Docker and cuckoo should be up and running by this point
-        ###
 
-        self.compare_ubuntu_versions()
+    def start_vm(self, vm_name, local_vnc_port=15900):
+        """
+        Starts a VM inside the docker container from the configured snapshot
+        and provides an SSH tunnel to the VNC port to view it
+
+        :param vm_name:
+        :return:
+        """
+
+        if self.service is None:
+            self.start_service()
+
+        cmd = """docker exec %s virsh snapshot-revert --current --domain %s""" % (self.service.cm.name, vm_name)
+        self.log.info("Trying to start VM inside docker with command: %s" % cmd)
+        stdout, stderr = self.service.cm._run_cmd(cmd, raise_on_error=False)
+
+        if len(stderr) > 0:
+            self.log.error("Problem starting VM, using command '%s'. "
+                           "Maybe an issue with libvirt inside the docker container? "
+                           "STDERR from command: %s" % (cmd, stderr))
+            return
+
+        cmd = """docker exec %s virsh vncdisplay %s""" % (self.service.cm.name, vm_name)
+        stdout, stderr = self.service.cm._run_cmd(cmd, raise_on_error=False)
+        if len(stderr) > 0:
+            self.log.error("Problem getting VNC display information using command '%s'" 
+                           "STDERR from command: %s" % (cmd, stderr))
+            return
+
+        stdout = stdout.strip()
+        docker_vnc_port = int(stdout.split(":")[1]) + 5900
+        self.log.info("VNC connection info (from within docker): %s, TCP port: %d" % (stdout, docker_vnc_port))
+
+        self.log.info("Creating temporary SSH key pair..")
+        ssh_keydir = tempfile.mkdtemp()
+        key = RSA.generate(2048)
+        pkey_path = os.path.join(ssh_keydir, "private.key")
+        pubkey_path = os.path.join(ssh_keydir, "public.key")
+        with open(pkey_path, 'w') as content_file:
+            os.chmod(pkey_path, 0600)
+            content_file.write(key.exportKey('PEM'))
+        pubkey = key.publickey()
+        with open(pubkey_path, 'w') as content_file:
+            content_file.write(pubkey.exportKey('OpenSSH'))
+
+        # dump the public key into the container
+        cmd = """docker exec %s bash -c "mkdir ~/.ssh && chmod 700 ~/.ssh && echo '%s' > ~/.ssh/authorized_keys" """ % (self.service.cm.name, pubkey.exportKey('OpenSSH'))
+        stdout, stderr = self.service.cm._run_cmd(cmd)
+
+        # Start the SSH server
+        cmd = """docker exec %s service ssh start""" % (self.service.cm.name)
+        stdout, stderr = self.service.cm._run_cmd(cmd)
 
 
-def do_main(sleep_loop=True):
-    logging.basicConfig(format=AL_LOG_FORMAT)
-    logger = logging.getLogger("assemblyline")
-    logger.setLevel(logging.DEBUG)
+        self.log.info("Trying to build SSH tunnel to VNC display...")
+        with sshtunnel.SSHTunnelForwarder(
+                (self.service.cuckoo_ip, 22),
+                ssh_username="root",
+                # ssh_pkey=pkey_path,
+                # TODO: upgrade paramiko in core?
+                # we have to do this hackey thing because we're pinned to an old
+                # version of paramiko
+                ssh_pkey = RSAKey.from_private_key_file(pkey_path),
+                # ssh_private_key_password="secret",
+                remote_bind_address=("localhost", docker_vnc_port),
+                local_bind_address=('0.0.0.0', local_vnc_port)
+            ) as tunnel:
+
+            self.log.info("Tunnel should be up. VNC to localhost:%d, or ssh -i %s/private.key root@%s" %
+                          (local_vnc_port, ssh_keydir, self.service.cuckoo_ip))
+            x = raw_input("Press any key and ENTER to continue...")
+
+        self.log.info("Cleaning up SSH keys...")
+        # cleanup
+        shutil.rmtree(ssh_keydir)
+
+
+def main(tests, sleep_loop=True):
+
 
     logger = logging.getLogger("assemblyline.cuckoo.testing.main")
 
     ct = CuckooTesting()
 
-    ct.start_service()
-    logger.info("Service started")
+    logger.info("Running tests %s" % ",".join(tests))
 
-    ct.run_tests()
+    for t in tests:
+        fn = getattr(ct, t)
+        fn()
 
-    if sleep_loop:
+    if sleep_loop and ct.service is not None:
         try:
             while True:
                 time.sleep(config.system.update_interval)
@@ -194,4 +402,34 @@ def do_main(sleep_loop=True):
         return ct
 
 if __name__ == "__main__":
-    do_main()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--help_tests", action="store_true", default=False,
+                        help="Display available tests and their associated descriptions")
+
+    parser.add_argument("-t", "--tests", nargs="+",
+                        help="The tests to run",
+                        choices = _AVAILABLE_TESTS)
+    parser.add_argument("-v", "--verbose", action="store_true", default=False,
+                        help="Verbose mode")
+
+    args = parser.parse_args()
+
+    logging.basicConfig(format=AL_LOG_FORMAT)
+    logger = logging.getLogger("assemblyline")
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    if args.help_tests:
+        for t in _AVAILABLE_TESTS:
+            fn = getattr(CuckooTesting, t)
+            print "Test: %s" % t
+            print "Description: %s" % fn.__doc__
+            print "============"
+
+    else:
+        # Do something more interesting...
+        # print args.tests
+        main(args.tests)
