ec47806dc09f3b4913ee2b5560ea99f012abf718
David <jdv.cse@gmail.com>
2018-08-13 15:59:29 -0400

    trying to use new version of inetsim / modded inetsim for random IPs

--- docker/cuckoobox/get_libs_for_cuckoo_docker.py
+++ docker/cuckoobox/get_libs_for_cuckoo_docker.py
@@ -9,7 +9,7 @@ from assemblyline.al.install import SiteInstaller
 alsi = SiteInstaller()
 
 library_files = ["volatility-2.6.zip",
-                 "inetsim_1.2.6-1_all.deb"]
+                 "inetsim_1.2.8-1_all.deb"]
 
 for lib in library_files:
     alsi.fetch_package("cuckoo/%s" % lib, "libs/%s" % lib)
