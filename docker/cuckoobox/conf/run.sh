cb7d51e8ced29ca9927141cbdb3f63eb173d6811
David <jdv.cse@gmail.com>
2018-09-07 15:51:31 -0400

    some more modifications to make new version of inetsim work

--- docker/cuckoobox/conf/run.sh
+++ docker/cuckoobox/conf/run.sh
@@ -9,4 +9,5 @@ find /var/lib/inetsim/ -name "*.mbox" -delete
 iptables -F
 iptables-restore /home/sandbox/conf/rules.v4
 
-/usr/bin/inetsim
+chmod g+r /var/lib/inetsim/certs/*
+/usr/bin/inetsim --config=/etc/inetsim/inetsim.conf --log-dir=/var/log/inetsim --data-dir=/var/lib/inetsim/
