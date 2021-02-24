a6a05241fbfd775e4ee9a3ee5e50628b2d2898db
Hardy-Cooper <Kevin.Hardy-Cooper@cyber.gc.ca>
2021-02-24 14:57:50 -0500

    Python 3 compatability only

--- cuckoo/cuckoo.py
+++ cuckoo/cuckoo.py
@@ -190,11 +190,7 @@ class SubmissionThread(Thread):
     def run(self):
         self.exc = None
         try:
-            if hasattr(self, '_Thread__target'):
-                # Thread uses name mangling prior to Python 3.
-                self.ret = self._Thread__target(*self._Thread__args, **self._Thread__kwargs)
-            else:
-                self.ret = self._target(*self._args, **self._kwargs)
+            self.ret = self._target(*self._args, **self._kwargs)
         except BaseException as e:
             self.exc = e
 
