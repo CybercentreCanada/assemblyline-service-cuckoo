6105f604910a5be6e5b13f513d5254a36c3e9c68
Kevin Hardy-Cooper <kevin.hardy-cooper@cyber.gc.ca>
2021-11-02 13:38:16 -0400

    Fix test

--- test/test_cuckoo.py
+++ test/test_cuckoo.py
@@ -3285,7 +3285,7 @@ class TestCuckooResult:
         _create_hashed_node(parent, node, hashes)
 
         for x in range(len(hashes)):
-            assert hashes[x].hexdigest() == expected_hashes[x]
+            assert hashes[x] == expected_hashes[x]
 
     @staticmethod
     @pytest.mark.parametrize(
