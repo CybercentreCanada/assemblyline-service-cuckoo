25e84eca1ac84154e38ebfcd880a829854b9e211
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2022-12-08 21:51:57 +0000

    Adding code/shell to files that Cuckoo supports

--- tests/test_cuckoo_main.py
+++ tests/test_cuckoo_main.py
@@ -370,7 +370,7 @@ class TestModule:
     def test_file_constants():
         from cuckoo.cuckoo_main import LINUX_x86_FILES, LINUX_x64_FILES, WINDOWS_x86_FILES
         assert set(LINUX_x86_FILES) == {"executable/linux/elf32", "executable/linux/so32", "executable/linux/coff32"}
-        assert set(LINUX_x64_FILES) == {"executable/linux/elf64", "executable/linux/so64", "executable/linux/ia/coff64", "executable/linux/coff64"}
+        assert set(LINUX_x64_FILES) == {"executable/linux/elf64", "executable/linux/so64", "executable/linux/ia/coff64", "executable/linux/coff64", "code/shell"}
         assert set(WINDOWS_x86_FILES) == {'executable/windows/pe32', 'executable/windows/dll32'}
 
     @staticmethod
